4d218e919a200a658028c2b130c8ece739866243
Admin <da8y_01@yahoo.com>
2017-08-16 09:51:31 -0500

    Usar variable de entorno.

--- dist/initBot.js
+++ dist/initBot.js
@@ -6,6 +6,7 @@ import {
 } from './bot/TelegramBot';
 */
 var TelegramBot = require('node-telegram-bot-api');
-var token = '393865087:AAHQJhT3VuzkGV9XUMr3wh8C2CpkIMY61bI';
+//var token = '393865087:AAHQJhT3VuzkGV9XUMr3wh8C2CpkIMY61bI';
+var token = process.env.TGBOTTOKEN;
 var bot = new TelegramBot(token, { polling: true });
 exports.bot = bot;
